5074e35838ad8ebccb5794e78d6c48879f1ef480
Pain31415 <Cherniy412@ukr.net>
2024-02-20 19:15:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the date in the format dd.mm.yyyy:");
+            DateTime date;
+
+            if (DateTime.TryParseExact(Console.ReadLine(), "dd.MM.yyyy", null, System.Globalization.DateTimeStyles.None, out date))
+            {
+                string season = GetSeason(date.Month);
+                string dayOfWeek = date.DayOfWeek.ToString();
+
+                Console.WriteLine($"{season} {dayOfWeek}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid date format.");
+            }
+        }
+
+        static string GetSeason(int month)
+        {
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    return "Winter";
+                case 3:
+                case 4:
+                case 5:
+                    return "Spring";
+                case 6:
+                case 7:
+                case 8:
+                    return "Summer";
+                case 9:
+                case 10:
+                case 11:
+                    return "Autumn";
+                default:
+                    return "Unknown";
+            }
+        }
+    }
+}
