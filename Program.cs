7db612f0ee0756a4d3871368f9392104d2b8a95f
Pain31415 <Cherniy412@ukr.net>
2024-02-20 19:15:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a six-digit number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int reversedNumber = 0;
+            while (number > 0)
+            {
+                int digit = number % 10;
+                reversedNumber = reversedNumber * 10 + digit;
+                number /= 10;
+            }
+
+            Console.WriteLine("Reversed number: " + reversedNumber);
+        }
+    }
+}
