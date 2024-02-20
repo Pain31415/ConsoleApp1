21ec5164e03dfd11842b205f62e3b6a14c327f9c
Pain31415 <Cherniy412@ukr.net>
2024-02-20 19:15:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter five numbers:");
+
+            double[] numbers = new double[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Number {i + 1}: ");
+                numbers[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            double sum = 0;
+            foreach (double num in numbers)
+            {
+                sum += num;
+            }
+
+            double max = numbers[0];
+            double min = numbers[0];
+            foreach (double num in numbers)
+            {
+                if (num > max)
+                {
+                    max = num;
+                }
+                if (num < min)
+                {
+                    min = num;
+                }
+            }
+
+            double product = 1;
+            foreach (double num in numbers)
+            {
+                product *= num;
+            }
+
+            Console.WriteLine($"Sum of numbers: {sum}");
+            Console.WriteLine($"Maximum: {max}");
+            Console.WriteLine($"Minimum: {min}");
+            Console.WriteLine($"Product of numbers: {product}");
+
+            Console.ReadLine();
+        }
+    }
+}
