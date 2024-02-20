0056e3a5ccff1d1eee97075e33fbd1d4593737e6
Pain31415 <Cherniy412@ukr.net>
2024-02-20 19:15:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the lower and upper bounds of the numerical range:");
+            int lowerBound = Convert.ToInt32(Console.ReadLine());
+            int upperBound = Convert.ToInt32(Console.ReadLine());
+
+            DisplayFibonacciInRange(lowerBound, upperBound);
+        }
+
+        static void DisplayFibonacciInRange(int lowerBound, int upperBound)
+        {
+            int a = 0, b = 1, c = 0;
+
+            if (lowerBound <= 0)
+            {
+                Console.Write("0, ");
+            }
+
+            while (c <= upperBound)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                if (c >= lowerBound && c <= upperBound)
+                {
+                    Console.Write(c + ", ");
+                }
+            }
+        }
+    }
+}
