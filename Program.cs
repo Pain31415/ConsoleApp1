f57070fdf4cbafa02d0741a920feb23096f2b92d
Pain31415 <Cherniy412@ukr.net>
2024-02-20 19:15:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number between 1 and 100:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number >= 1 && number <= 100)
+            {
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (number % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (number % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Number is not between 1 and 100.");
+            }
+        }
+    }
+}
