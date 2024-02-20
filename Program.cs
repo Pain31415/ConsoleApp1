a22b5330d4d11f814f99bcba591cf220c8178125
Pain31415 <Cherniy412@ukr.net>
2024-02-20 19:15:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string quote = "It's easy to win forgiveness for being wrong; " +
+                           "being right is what gets you into real trouble.";
+            string author = "Bjarne Stroustrup";
+
+            Console.WriteLine(quote);
+            Console.WriteLine(author);
+        }
+    }
+}
