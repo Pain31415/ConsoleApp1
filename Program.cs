4cb717a3699f0eccef51c1ba3bf0a3b8ce8d2e48
Pain31415 <Cherniy412@ukr.net>
2024-02-20 19:15:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter four digits:");
+
+            int digit1 = Convert.ToInt32(Console.ReadLine());
+            int digit2 = Convert.ToInt32(Console.ReadLine());
+            int digit3 = Convert.ToInt32(Console.ReadLine());
+            int digit4 = Convert.ToInt32(Console.ReadLine());
+
+            int number = digit1 * 1000 + digit2 * 100 + digit3 * 10 + digit4;
+
+            Console.WriteLine($"Number composed of the digits: {number}");
+        }
+    }
+}
