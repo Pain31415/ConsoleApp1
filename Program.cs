9184bc074a4cd3f76ce7cad357b80de276d8747f
Pain31415 <Cherniy412@ukr.net>
2024-02-20 19:15:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number:");
+            double number = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the percentage:");
+            double percentage = Convert.ToDouble(Console.ReadLine());
+
+            double result = (percentage / 100) * number;
+
+            Console.WriteLine($"Result: {result}");
+        }
+    }
+}
